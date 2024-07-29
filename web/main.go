0db66b6eed03221db290caf65cce9399cc421de8
andyosyndoh <andyovvo8@gmail.com>
2024-07-29 22:56:04 +0300

    chore : added func register routes with routes

--- web/main.go
+++ web/main.go
@@ -6,11 +6,6 @@ import (
 	"net/http"
 )
 
-// homeHandler is a handler function for the root path
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
-}
-
 func main() {
 	mux := http.NewServeMux()
 	routes.RegisterRoutes(mux)
