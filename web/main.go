2bb950bfc9f950469cd728e27b818c01fd38de55
andyosyndoh <andyovvo8@gmail.com>
2024-07-29 22:39:06 +0300

    refactor: refactored main

--- web/main.go
+++ web/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 // homeHandler is a handler function for the root path
@@ -13,9 +12,19 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", homeHandler).Methods("GET")
+	mux := http.NewServeMux()
+	routes.RegisterRoutes(Smux)
 
-	log.Println("Listening on port 3000...")
-	log.Fatal(http.ListenAndServe(":3000", r))
+	wrappedMux := routes.RouteChecker(mux)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: wrappedMux,
+	}
+
+	fmt.Println("server running @http://localhost:8080")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
