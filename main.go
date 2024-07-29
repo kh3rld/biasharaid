9cd53cf78f1a8057426f533b2ae5a33c16e04ae1
adiozdaniel <adiozdaniel@gmail.com>
2024-07-29 21:16:34 +0300

    chore: add a homehandler - this handler handles only the get requests

--- main.go
+++ main.go
@@ -7,9 +7,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// homeHandler is a handler function for the root path
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
+}
+
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", nil).Methods("GET")
+	r.HandleFunc("/", homeHandler).Methods("GET")
 
 	log.Println("Listening on port 3000...")
 	log.Fatal(http.ListenAndServe(":3000", r))
