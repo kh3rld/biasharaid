a6aadc3a0dea8dfdd701d033be0750047d2e67c0
andyosyndoh <andyovvo8@gmail.com>
2024-07-29 23:35:34 +0300

    chore : added handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,3 +5,16 @@ import "net/http"
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	renders.RenderTemplate(w, "home.page.html", nil)
 }
+
+func Verification(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Details(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	renders.RenderTemplate(w, "notfound.page.html", nil)
+}
