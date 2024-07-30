bfec0851864d54d02167cdfb6a92a5c31379b427
adiozdaniel <adiozdaniel@gmail.com>
2024-07-30 18:07:39 +0300

    fix: change the notfound page

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -58,7 +58,7 @@ func VerifyHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if block == nil {
-			renders.RenderTemplate(w, "404.html", nil)
+			renders.RenderTemplate(w, "404.page.html", nil)
 			return
 		}
 
